dc33a8e014d030f2c9250761912e3532d66e0a6f
Pawel Kowalczuk <pkowalczuk@applause.com>
2018-09-26 17:57:55 +0200

    first commit

---
+++ Models/Acronym.swift
@@ -0,0 +1,34 @@
+//
+//  Acronym.swift
+//  App
+//
+//  Created by Pawel Kowalczuk on 26/09/2018.
+//
+
+import Foundation
+import FluentSQLite
+import Vapor
+
+final class Acronym: Codable {
+    var id: Int?
+    var short: String
+    var long: String
+
+    init(short: String, long: String) {
+        self.short = short
+        self.long = long
+    }
+}
+
+extension Acronym: Model {
+
+    typealias Database = SQLiteDatabase
+    typealias ID = Int
+
+    public static var idKey: IDKey = \Acronym.id
+}
+
+extension Acronym: SQLiteModel {}
+extension Acronym: Migration {}
+extension Acronym: Content {}
+
